03e58f8c9372e3a56eca9417c4aaf6acb5f23212
HugoEklund <eklund.hugo@gmail.com>
2023-09-28 11:47:54 +0200

    waaa

--- Labb1.2/MainSimulation2.java
+++ Labb1.2/MainSimulation2.java
@@ -7,16 +7,16 @@ public class MainSimulation2 extends GlobalSimulation2{
     	Event2 actEvent;
     	EventList2 myEventList = new EventList2();
     	State2 actState = new State2(myEventList);
-        myEventList.InsertEvent(ARRIVAL_A, 0);
-		myEventList.InsertEvent(ARRIVAL_B, 0);
-        myEventList.InsertEvent(MEASURE, 5);
-    	while (time < 105)
+        myEventList.InsertEvent(ARRIVAL_A, 1);
+        myEventList.InsertEvent(MEASURE, 0);
+
+    	while (time < 100)
 		{
     		actEvent = myEventList.FetchEvent();
     		time = actEvent.eventTime;
     		actState.TreatEvent(actEvent);
     	}
-    	System.out.println("Mean number of customers in server + buffer: " + 0.1*actState.accumulated/actState.noMeasurements);
+    	System.out.println("Mean number of customers in server + buffer: " + 1.0 * actState.accumulated/actState.noMeasurements);
     	System.out.println("Number of measurements done: " + actState.noMeasurements);
     }
 }
--- Labb1.2/State2.java
+++ Labb1.2/State2.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class State2 extends GlobalSimulation2{
 	
-	public int nbrInQA = 0, nbrInQB = 0, accumulated = 0, noMeasurements = 0;
+	public int a_Queue = 0, b_Queue = 0, accumulated = 0, noMeasurements = 0;
 	
 	private EventList2 myEventList;
 
@@ -12,12 +12,13 @@ class State2 extends GlobalSimulation2{
 		myEventList = x;
 	}
 	
-	private void InsertEvent(int event, double timeOfEvent){
+	private void InsertEvent(int event, double timeOfEvent)
+	{
 		myEventList.InsertEvent(event, timeOfEvent);
 	}
 	
-	
-	public void TreatEvent(Event2 x){
+	public void TreatEvent(Event2 x)
+	{
 		switch (x.eventType){
 			case ARRIVAL_A:
 				arrival_A();
@@ -37,44 +38,67 @@ class State2 extends GlobalSimulation2{
 		}
 	}
 	
-	private double generateMean(double mean){
+	private double generateMean(double mean)
+	{
 		return 2*mean*slump.nextDouble();
 	}
 	
-	public double intervall() {
-		double nbr = slump.nextDouble(0.2 , 150);
-		return 1/nbr;
+	public double arrivalInterval() 
+	{
+		double nbr = slump.nextDouble(2.0/150.0);
+		return nbr;
 	}
 	
-	private void arrival_A(){
-		if (nbrInQA == 0 && nbrInQB == 0)
-			InsertEvent(READY_A, time + 0.002); // betjäning alltid 2 ms
-		nbrInQA++;
-		InsertEvent(ARRIVAL_A, time + generateMean(intervall())); // inkomst 1/150 ≈ 0.00667
-	}
-	
-	private void arrival_B() {
-		if(nbrInQB == 0) { 
-			InsertEvent(READY_B, time + 0.004); // betjäning alltid 4 ms
-		nbrInQB++;
-		InsertEvent(ARRIVAL_B, time + 1); // inkomst alltid 1 
+	private void arrival_A()
+	{
+		if (b_Queue == 0)
+		{
+			InsertEvent(READY_A, time + 0.002);
+			a_Queue++;
+			InsertEvent(ARRIVAL_A, time + generateMean(arrivalInterval()));
 		}
 	}
 	
-	private void ready_A(){
-		nbrInQA--;
-		if (nbrInQA > 0)
-			InsertEvent(READY_A, time + 1); //förbindelse alltid 1s
+	private void arrival_B() 
+	{
+		if (a_Queue == 0 && b_Queue == 0)
+		{
+			InsertEvent(READY_B, time + 0.004);
+			b_Queue++;
+		}
 	}
 	
-	private void ready_B() {
-		nbrInQB--;
-		if(nbrInQB > 0)
-			InsertEvent(READY_B, time + 1); //förbindelse alltid 1s
+	private void ready_A()
+	{
+		a_Queue--;
+		if (b_Queue > 0)
+		{
+			InsertEvent(READY_A, time + 0.002);
+		}
+		else if(a_Queue > 0)
+		{
+			InsertEvent(READY_B, time + 0.004);
+		}
+		InsertEvent(ARRIVAL_B, time + 1);
+	}
+
+	private void ready_B()
+	{
+		b_Queue--;
+		if (b_Queue > 0)
+		{
+			InsertEvent(READY_B, time + 0.002);
+		}
+		else if(a_Queue > 0)
+		{
+			InsertEvent(READY_A, time + 0.004);
+		}
+		InsertEvent(ARRIVAL_B, time + 1);
 	}
 	
-	private void measure(){
-		accumulated += nbrInQA + nbrInQB;
+	private void measure()
+	{
+		accumulated += a_Queue + b_Queue;
 		noMeasurements++;
 		InsertEvent(MEASURE, time + 0.1);
 	}
