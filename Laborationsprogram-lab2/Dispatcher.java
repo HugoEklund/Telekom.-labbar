d156901a9ae30e94b915ef0acf99d6fd28024bdf
HugoEklund <60035732+HugoEklund@users.noreply.github.com>
2023-10-03 18:14:31 +0200

    Add files via upload

---
+++ Laborationsprogram-lab2/Dispatcher.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Dispatcher extends Gen 
+{
+    public double lambda;
+    public ArrayList<QS> sendToQueue;
+
+	public Dispatcher(ArrayList<QS> qs, double lambda)
+	{
+        this.sendToQueue = qs;
+        this.lambda = lambda;
+	}
+    
+    public void TreatSignal(Signal x) 
+    {
+        switch (x.signalType) 
+        {
+            case READY:
+            {
+                Random slump = new Random();
+                int rngQueue = slump.nextInt(5);
+                SignalList.SendSignal(ARRIVAL, sendToQueue.get(rngQueue), time);
+                SignalList.SendSignal(READY, this, time + (2.0 / lambda) * slump.nextDouble());
+            }
+            break;
+        }
+    }
+}
